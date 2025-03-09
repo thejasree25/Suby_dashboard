e56360dd22b0754001f51a753cf1c46ae881a2aa
thejasree51 <gthejasree81@gmail.com>
2025-03-09 11:57:18 +0530

    changed path to live in api url

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,2 +1,3 @@
- export const API_URL= "http://localhost:4000"
- console.log("API URL:", API_URL);
+//  export const API_URL= "http://localhost:4000"
+//  console.log("API URL:", API_URL);
+export const API_URL="https://backend-8l7s.onrender.com"
