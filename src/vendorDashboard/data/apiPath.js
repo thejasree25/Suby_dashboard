77040f7c0c73ad1cd8e3650e9aa44440fb33ff5d
thejasree51 <gthejasree81@gmail.com>
2025-03-12 10:58:39 +0530

    first commit

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,3 +1,3 @@
  export const API_URL= "http://localhost:4000"
 //  console.log("API URL:", API_URL);
-// export const API_URL="https://backend-8l7s.onrender.com"
+// export const API_URL="https://backend-project-74d2.onrender.com"
